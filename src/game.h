7baaa4e9c8a4da25aaed77e3147756dcdf79c4df
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-02-26 22:43:54 +0100

    1.0

--- src/game.h
+++ src/game.h
@@ -1,4 +1,30 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include"def.h"
+#include <iostream>
+
+class game
+{
+private:
+    /* data */
+    bool isRunning;
+    SDL_Window * window;
+    SDL_Renderer *renderer;
+
+public:
+    game();
+    ~game();
+
+    void init(const char* title, int posX, int posY, int width, int height, bool fullscreen);
+    
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running();
+};
+
+
 #endif 
