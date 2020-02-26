ed1fc032e178cdfedc5bd6608d046a545b13eed8
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-02-26 22:46:16 +0100

    remise a 0

--- src/terrain.h
+++ src/terrain.h
@@ -1,4 +1,20 @@
 #ifndef TERRAIN_H
 #define TERRAIN_H
 
+#include "obstacle.h"
+
+class terrain
+{
+private:
+    /* data */
+    obstacle * tab ;
+    
+public:
+    terrain();
+    ~terrain();
+    void affichage() ;
+};
+ 
+
+
 #endif 
