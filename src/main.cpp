23d6db04765d58954ce01bce31739fc9c5a23183
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-02-26 22:44:20 +0100

    affichage de l'ecran

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,25 @@
-#include "def.h";
+#include "game.h" 
 
-int main(int argc, char** argv)
-{
-    /* Initialisation simple */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+int main(int argc, const char* argv[]) {
+    
+    game g ;
+
+    g.init("écran", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, false);
+
+    while (g.running()) //game is running 
     {
-        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
-        return -1;
+        /* handle any user input
+        update all object...
+        render changes to the display */
+
+        g.handleEvents();
+        g.update();
+        g.render();
+
     }
-
-    {
-        /* Création de la fenêtre */
-        SDL_Window* pWindow = NULL;
-        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                                  SDL_WINDOWPOS_UNDEFINED,
-                                                                  640,
-                                                                  480,
-                                                                  SDL_WINDOW_SHOWN);
-
-        if( pWindow )
-        {
-            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
-
-            SDL_DestroyWindow(pWindow);
-        }
-        else
-        {
-            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
-        }
-    }
-
-    SDL_Quit();
+    
+    g.clean();
 
     return 0;
+
 }
