cff539a09f0087b322fff2624ab8f3dc0b8ca097
William <william.escriva@etu.univ-lyon1.fr>
2020-03-02 20:28:38 +0100

    pion qui bouge dans le bon sens,besoin du mutlitouche

--- src/game.cpp
+++ src/game.cpp
@@ -32,32 +32,63 @@ void game::init(const char* title, int posX, int posY, int width, int height, bo
     } else {
         isRunning = false ;
     }
+    joueur=new personnage;
+    SDL_Surface* tmpsurface=IMG_Load("Data/asset.png");
+    joueur->setTexture(renderer,tmpsurface);
+    SDL_FreeSurface(tmpsurface);
 }
 
 void game::handleEvents()
 {
-    SDL_Event event;
-    SDL_PollEvent(&event);
-    switch (event.type)
-    {
-    case SDL_QUIT:
-        isRunning = false ;
-        break;
-    
-    default:
-        break;
+    SDL_Event events;
+    SDL_PollEvent(&events);
+    float f=0;
+    double ang=0;
+    if (events.type == SDL_KEYDOWN) { // Si une touche est enfoncee
+        switch (events.key.keysym.sym) {
+        case SDLK_z:
+            f=1;
+            break;
+        case SDLK_s:
+            f=-1;
+            break;
+        case SDLK_q:
+            ang=1;
+            break;
+        case SDLK_d:
+            ang=-1;
+            break;
+        case SDLK_ESCAPE:
+            isRunning=false;
+            break;
+        default: break;
+        }
     }
+    joueur->deplace(f,ang);
+    if (events.type == SDL_KEYUP) { // Si une touche est relaché
+        switch (events.key.keysym.sym) {
+        case SDLK_z:
+            f=0;
+            break;
+        case SDLK_s:
+            f=0;
+            break;
+        default: break;
+        }
+    }
+    if (events.type == SDL_QUIT) isRunning=false;// Si l'utilisateur a clique sur la croix de fermeture
 }
 
 void game::update()
 {
-
+    joueur->update();
 }
 
 void game::render()
 {
     SDL_RenderClear(renderer);
     //this is where we would add stuff to render
+    SDL_RenderCopyEx(renderer,joueur->getTexture(),NULL,joueur->getRect(),-joueur->getAngle(),NULL,SDL_FLIP_NONE);
     SDL_RenderPresent(renderer);
 }
 
