2bc7d9b95e70292d12738a42816cddfd217783cd
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-03-17 12:00:35 +0100

    ce module permet d'afficher des texture

---
+++ src/texture.h
@@ -0,0 +1,30 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#include "def.h" 
+
+class texture
+{
+private:
+    
+   const char *fileName ;
+   SDL_Texture * tex;
+
+
+public:
+    
+    texture();
+    SDL_Texture* loadTexture(const char *file, SDL_Renderer *ren);
+    void renderTexture(SDL_Texture*tex, SDL_Renderer *ren, int x, int y);
+    
+    const char *getFileName();
+    void setFileName(char * file);
+
+    SDL_Texture *getTexture();
+    void setTexture(SDL_Texture *t);
+
+};
+
+
+
+
+#endif
