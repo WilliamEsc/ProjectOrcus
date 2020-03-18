0c577d0481acec0d88354daf9c141bc18ac87db3
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-03-18 23:39:20 +0100

    maj

--- src/obstacle.h
+++ src/obstacle.h
@@ -1,4 +1,20 @@
 #ifndef OBSATCLE_H
 #define OBSATCLE_H
 
+    class obstacle
+    {
+    private:
+        /* data */
+        int posX ;
+        int posY ;
+    
+    public:
+        obstacle(/* args */);
+        obstacle(int positionX, int positionY);
+        ~obstacle();
+    };
+    
+    
+    
+
 #endif 
