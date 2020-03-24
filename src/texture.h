525e982b8777f1da88a1fd4729604026ba72bfe5
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-03-24 13:32:06 +0100

    maj

--- src/texture.h
+++ src/texture.h
@@ -8,20 +8,23 @@ private:
     
    const char *fileName ;
    SDL_Texture * tex;
-
+   SDL_Rect dest; 
 
 public:
     
     texture();
     SDL_Texture* loadTexture(const char *file, SDL_Renderer *ren);
     SDL_Texture* loadTextureText(const char * texte, TTF_Font *font, SDL_Color color, SDL_Renderer *ren);
-    void renderTexture(SDL_Texture*tex, SDL_Renderer *ren, int x, int y);
+    void renderTexture(SDL_Texture*tex, SDL_Renderer *ren, SDL_Rect dst, float x, float y);
     
     const char *getFileName();
     void setFileName(char * file);
 
     SDL_Texture *getTexture();
     void setTexture(SDL_Texture *t);
+
+    SDL_Rect* getRect();
+    SDL_Rect getRect2();
 
 };
 
