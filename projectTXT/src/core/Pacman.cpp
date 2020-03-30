7b1fd9a62b8d961e73b74d7dddb200985d09b499
William <william.escriva@etu.univ-lyon1.fr>
2020-03-30 17:46:43 +0200

    fix diagonal

--- projectTXT/src/core/Pacman.cpp
+++ projectTXT/src/core/Pacman.cpp
@@ -4,8 +4,9 @@ Pacman::Pacman () : Personnage(){
  	x = 1 ;
 	y = 2 ;	
 	pdv = 50 ;
-	a.setPos(x,y+1);
-	a.setInc(x,y);
+	a.setPos(x,y);
+	a.setInc(0,1);
+	a.setSav(0,1);
 }
 
 void Pacman::setPdv (int p)
@@ -40,37 +41,41 @@ void Pacman::updateBalle(const Terrain & t,Fantome& fan,Fantome& fan2){
 
 void Pacman::canonG(){
 	if(!a.getPop()){
-		a.setPos(x-1,y);
-		a.setInc(x,y);}
+		a.setPos(x,y);
+		a.setInc(-1,0);
+		a.setSav(-1,0);}
 	else{
-		a.setSav(x+1,y);
+		a.setSav(-1,0);
 	}
 }
 
 void Pacman::canonD(){
 	if(!a.getPop()){
-	a.setPos(x+1,y);
-	a.setInc(x,y);}
+	a.setPos(x,y);
+	a.setInc(1,0);
+	a.setSav(1,0);}
 	else{
-		a.setSav(x-1,y);
+		a.setSav(1,0);
 	}
 }
 
 void Pacman::canonH(){
 	if(!a.getPop()){
-	a.setPos(x,y-1);
-	a.setInc(x,y);}
+	a.setPos(x,y);
+	a.setInc(0,-1);
+	a.setSav(0,-1);}
 	else{
-		a.setSav(x,y+1);
+		a.setSav(0,-1);
 	}
 }
 
 void Pacman::canonB(){
 	if(!a.getPop()){
-	a.setPos(x,y+1);
-	a.setInc(x,y);}
+	a.setPos(x,y);
+	a.setInc(0,1);
+	a.setSav(0,1);}
 	else{
-		a.setSav(x,y-1);
+		a.setSav(0,1);
 	}
 }
 
@@ -81,6 +86,24 @@ char* Pacman::affichePdv(char *str)const
 	return str ;
 }
 
+ void Pacman::testRegression(){
+	
+	int i=1;
+	
+	assert(x==1 && y==2 && pdv==50 && a.getPosX()==x && a.getPosY()==y+1 && a.getIncX()==0 && a.getIncY()==1 && !a.getPop());
+	std::cout << "test " << i << " reussi" << std::endl ;
+    i++ ;
 
+	setPdv(99);
+	assert(pdv==99);
+	std::cout << "test " << i << " reussi" << std::endl ;
+    i++ ;
+
+	Arme b=getArme();
+
+	assert(b.getPosX()==a.getPosX() && b.getPosY()==a.getPosY() && b.getIncX()==a.getIncX() && b.getIncY()==a.getIncY() && !b.getPop());
+	std::cout << "test " << i << " reussi" << std::endl ;
+    i++ ;
+ }
 
 
