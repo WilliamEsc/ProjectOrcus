74e144749237ae5ae581dbcea92ae198a15011cf
William <william.escriva@etu.univ-lyon1.fr>
2020-03-30 16:07:54 +0200

    fix du tir

--- projectTXT/src/core/Arme.cpp
+++ projectTXT/src/core/Arme.cpp
@@ -2,6 +2,8 @@
 
 Arme::Arme(){
     pop=false;
+    Savx=0;
+    Savy=0;
 }
 
 void Arme::setPos(int x,int y){
@@ -12,6 +14,11 @@ void Arme::setPos(int x,int y){
 void Arme::setInc(int x,int y){
     Incx=tx-x;
     Incy=ty-y;
+}
+
+void Arme::setSav(int x,int y){
+    Savx=x;
+    Savy=y;
 }
 
 void Arme::setPop(bool b){
@@ -38,17 +45,26 @@ bool Arme::getPop()const{
     return pop;
 }
 
-void Arme::bougeBalle(const Terrain & t,int fanx,int fany,bool & fanb, int fan2x,int fan2y,bool & fan2b){
+void Arme::bougeBalle(const Terrain & t,int fanx,int fany,bool & fanb, int fan2x,int fan2y,bool & fan2b,const int & pacx,const int & pacy){
     if(t.estPositionPersoValide(tx+Incx,ty+Incy) && pop){
     tx+=Incx;
     ty+=Incy;}
     else{
         pop=false;
+        tx=pacx;
+        ty=pacy;
+        setInc(Savx,Savy);
     }
-    if(tx==fanx && ty==fany){
+    if(tx==fanx && ty==fany && fanb){
         pop=false;
-        fanb=false;}
-    if(tx==fan2x && ty==fan2y){
+        fanb=false;
+        tx=pacx;
+        ty=pacy;
+        setInc(Savx,Savy);}
+    if(tx==fan2x && ty==fan2y && fan2b){
         pop=false;
-        fan2b=false;}
+        fan2b=false;
+        tx=pacx;
+        ty=pacy;
+        setInc(Savx,Savy);}
 }
