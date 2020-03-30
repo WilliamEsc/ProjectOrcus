0091b50a8e1e9fdb87e3df126ac5b9dad06416b0
William <william.escriva@etu.univ-lyon1.fr>
2020-03-30 13:06:32 +0200

    ajout du tir

--- projectTXT/src/core/Fantome.cpp
+++ projectTXT/src/core/Fantome.cpp
@@ -1,9 +1,9 @@
 #include "Fantome.h"
-#include <stdlib.h>
 
 Fantome::Fantome () {
 	x = y = 10;
 	dir = 0;
+    pop=true;
 }
 
 Fantome::Fantome(int PosX, int PosY)
@@ -11,9 +11,11 @@ Fantome::Fantome(int PosX, int PosY)
     x = PosX ;
     y = PosY ;
     dir = 0 ;
+    pop=true;
 }
 
 void Fantome::versPacman (const Terrain & t, const Pacman & p) {
+    if(pop){
     int dx = p.getX()-x;
     int dy = p.getY()-y;
     if (dx>0) dx=1;
@@ -24,9 +26,11 @@ void Fantome::versPacman (const Terrain & t, const Pacman & p) {
         x = x+dx;
         y = y+dy;
     }
+    }
 }
 
 void Fantome::bougeAuto (const Terrain & t) {
+    if(pop){
     int dx [4] = { 1, 0, -1, 0};
     int dy [4] = { 0, 1, 0, -1};
     int xtmp,ytmp;
@@ -36,5 +40,12 @@ void Fantome::bougeAuto (const Terrain & t) {
         x = xtmp;
         y = ytmp;
     }
-    else dir = rand()%4;
+    else dir = rand()%4;}
+}
+
+void Fantome::setPop(bool b){
+    pop=b;
+}
+bool Fantome::getPop()const{
+    return pop;
 }
