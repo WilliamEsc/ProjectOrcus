87cffe6a204261be44307fcb85b9457463179af8
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-03-30 15:58:28 +0200

    maj

--- projectTXT/src/mainRegression.cpp
+++ projectTXT/src/mainRegression.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include "core/Fantome.h"
+#include "core/Soin.h"
+
+using namespace std ;
 
 int main () 
 {
     Fantome fan ;
+    Soin s;
+
+    cout << "Test Regression Fantome :" << endl ;
     fan.testRegression();
+
+    cout << "Test Regression Soin :" << endl ;
+    s.testRegression();
+
     return 0 ;
 }
