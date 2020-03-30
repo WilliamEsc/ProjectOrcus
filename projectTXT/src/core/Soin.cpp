c12f7dabd708547940c0f645b6ff2c71e4ad8978
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-03-30 15:58:45 +0200

    maj

--- projectTXT/src/core/Soin.cpp
+++ projectTXT/src/core/Soin.cpp
@@ -16,3 +16,26 @@ void Soin :: hitPac(Pacman &p)
 {
     p.setPdv(p.getPdv()-5);
 }
+
+void Soin :: testRegression()
+{
+    Soin s ;
+    Pacman p;
+    int i = 1 ;
+
+    assert(s.x == 10 && s.y == 20 && s.heal == 10);
+    std::cout << "test " << i << " reussi" << std::endl ;
+    i++ ;
+    
+    int t = p.getPdv();
+    s.soignerPac(p);
+    assert(p.getPdv() == t+10);
+    std::cout << "test " << i << " reussi" << std::endl ;
+    i++ ;
+
+    t = p.getPdv();
+    s.hitPac(p);
+    assert(p.getPdv() == t-5);
+    std::cout << "test " << i << " reussi" << std::endl ;
+    i++ ;
+}
