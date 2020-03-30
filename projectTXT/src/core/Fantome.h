ca203a74e02ccbd3024c990d9bac088e9013510f
William <william.escriva@etu.univ-lyon1.fr>
2020-03-30 13:06:35 +0200

    ajout du tir

--- projectTXT/src/core/Fantome.h
+++ projectTXT/src/core/Fantome.h
@@ -7,26 +7,33 @@
 #ifndef _FANTOME_H
 #define _FANTOME_H
 
+#include <stdlib.h>
+
+#include "Pacman.h"
 #include "Terrain.h"
 #include "Personnage.h"
-#include "Pacman.h"
+
 
 
 /**
 @brief Un fantome = sa position 2D
 */
+class Pacman;
 class Fantome : public Personnage {
 
 protected :
 
 	//int x,y;
 	int dir;
+    bool pop;
 
 public:
 
     Fantome();
     Fantome(int x, int y);
 
+    void setPop(bool b);
+    bool getPop()const;
     void versPacman (const Terrain & t, const Pacman & p);
     void bougeAuto (const Terrain & t);
 
