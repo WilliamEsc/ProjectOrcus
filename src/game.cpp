fd4944a96084afe5e916e07e92b29ae1ae684808
William <william.escriva@etu.univ-lyon1.fr>
2020-03-31 09:19:40 +0200

    ajout carte

--- src/game.cpp
+++ src/game.cpp
@@ -4,7 +4,7 @@
 
 game::game(){};
 game::~game(){};
- map* m;
+ map m;
 
  
 
@@ -41,69 +41,68 @@ void game::init(const char* title, int posX, int posY, int width, int height, bo
     {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", TTF_GetError());
     }
-
-    joueur=new personnage;
-    SDL_Surface* tmpsurface=IMG_Load("Data/asset.png");
-    joueur->setTexture(renderer,tmpsurface);
+    SDL_Surface* tmpsurface=IMG_Load("Data/persos1.png");
+    joueur.setTexture(renderer,tmpsurface);
     SDL_FreeSurface(tmpsurface);
-    m = new map(renderer);
-    s = new soin();
-    s->setPos(384,284);
-    t = new text() ;
+    map m2(renderer);
+    m=m2;
+    s.setPos(25,25);
+    // t = new text() ;
 }
 
 void game::handleEvents()
 {
     SDL_Event events;
     SDL_PollEvent(&events);
-    float f;
-    double ang;
+    float f=0;
+    double ang=0;
     const Uint8* keystates;
     keystates = SDL_GetKeyboardState(NULL);
+    
     if (keystates[SDL_SCANCODE_W]) {
-        f=0.6;
+        f=0.01;
     }
     if (keystates[SDL_SCANCODE_S]) {
-        f=-0.6;
+        f=-0.01;
     }
     if (keystates[SDL_SCANCODE_A]) {
-        ang=0.3;
+        ang=0.6;
     }
     if (keystates[SDL_SCANCODE_D]) {
-       ang=-0.3;
+       ang=-0.6;
     }
     if (keystates[SDL_SCANCODE_ESCAPE]){
         isRunning=false;
     }
-    joueur->deplace(f,ang);
+    joueur.deplace(f,ang);
 
     if (events.type == SDL_QUIT) isRunning=false;// Si l'utilisateur a clique sur la croix de fermeture
     if (keystates[SDL_SCANCODE_X])
     {
-         //s->setPointDeVie(s->getPointDeVie()-10);
-        //std::cout << "Point de vie : " << s->getPointDeVie() << std::endl ;
-        std::cout << "pdv : " << joueur->getPdv() << std::endl ;
+         //s.setPointDeVie(s.getPointDeVie()-10);
+        //std::cout << "Point de vie : " << s.getPointDeVie() << std::endl ;
+        std::cout << "pdv : " << joueur.getPdv() << std::endl ;
 
-        std::cout << "position perso : " << joueur->getPos()->getComplexX() <<" "<< joueur->getPos()->getComplexY() << std::endl ;
-        std::cout << "position kit x: " << s->getPosX() << "y :"<< s->getPosY() << std::endl ;
+        std::cout << "position perso : " << joueur.getPos()->getComplexX() <<" "<< joueur.getPos()->getComplexY() << std::endl ;
+        std::cout << "position kit x: " << s.getPosX() << "y :"<< s.getPosY() << std::endl ;
     }
 }
 
 void game::heal()
 {
-     //SDL_bool col = SDL_HasIntersection(joueur->getRect(), s->getRect());
+     //SDL_bool col = SDL_HasIntersection(joueur.getRect(), s.getRect());
 }
 
 void game::update()
 {
-    joueur->update();
-    Complex pos= *joueur->getPos();
-    Complex posS(s->getPosX(),s->getPosY());
+    joueur.update();
+    Complex pos= *joueur.getPos();
+    Complex posS(s.getPosX(),s.getPosY());
     pos.Soustrait(&posS);
-    if(pos.norme()<50 && s->getPop())
+    if(pos.norme()<50 && s.getPop())
     {
-        joueur->setPdv(joueur->getPdv()+10);
-        s->setPop(false);
+        joueur.setPdv(joueur.getPdv()+10);
+        s.setPop(false);
     }
 }
 
@@ -111,17 +110,16 @@ void game::render()
 {
     SDL_RenderClear(renderer);
     //this is where we would add stuff to render
-    m->drawMap(renderer);
-    if(s->getPop())
-    s->setTexture(renderer, s->getPosX(), s->getPosY());
-    t->setTexte("Point de vie",renderer,0,0);
-    std::string txtPdv=std::to_string(joueur->getPdv());
-    t->setTexte(txtPdv.c_str(),renderer,0,20);
-    SDL_RenderCopyEx(renderer,joueur->getTexture(),NULL,joueur->getRect(),-joueur->getAngle(),NULL,SDL_FLIP_NONE);
-
-    //TTF_CloseFont(t->getFont());
+    m.drawMap(renderer,*joueur.getPos());
+    if(s.getPop())
+    s.setTexture(renderer, s.getPosX(), s.getPosY());
+    // t.setTexte("Point de vie",renderer,0,0);
+    // std::string txtPdv=std::to_string(joueur.getPdv());
+    // t.setTexte(txtPdv.c_str(),renderer,0,20);
+    //SDL_RenderCopy(renderer,joueur.getTexture(),NULL,joueur.getRect());
+    SDL_RenderCopyEx(renderer,joueur.getTexture(),NULL,joueur.getRect(),-joueur.getAngle(),NULL,SDL_FLIP_NONE);
+    // TTF_CloseFont(t.getFont());
     SDL_RenderPresent(renderer);
-    
 }
 
 void game::clean()
