c65dd2180874e09630020ce1f4f1a01db16d2d05
William <william.escriva@etu.univ-lyon1.fr>
2020-03-31 09:18:57 +0200

    ajout carte

--- src/personnage.cpp
+++ src/personnage.cpp
@@ -5,10 +5,17 @@ personnage::personnage(){
     vit=new Complex(20,21);
     angle=0;
     pdv=100;
-    destRect.h=128;
-    destRect.w=128;
-    destRect.x=pos->getComplexX();
-    destRect.y=pos->getComplexY();
+    destRect.h=32;
+    destRect.w=32;
+    destRect.x=400;
+    destRect.y=300;
+}
+
+personnage::~personnage(){
+    delete pos;
+    delete vit;
+    pos=NULL;
+    vit=NULL;
 }
 
 SDL_Texture* personnage::getTexture()const {
@@ -41,6 +48,11 @@ void personnage::setPdv(int n){
 
 void personnage::setTexture(SDL_Renderer* renderer,SDL_Surface* surface){
     Pion=SDL_CreateTextureFromSurface(renderer,surface);
+    if(Pion==NULL)
+    {
+        printf("erreur Pion");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void personnage::ajoutAngle(double deg){
@@ -51,12 +63,31 @@ void personnage::ajoutAngle(double deg){
 
 void personnage::deplace(float f,double ang){
     ajoutAngle(ang);
-    Complex tr=*vit-*pos;
-    *pos=*pos-tr*f;
-    *vit=tr+*pos;
+    Complex postmp= *pos;
+    Complex tr=*vit-postmp;
+    postmp=postmp-tr*f;
+    if(postmp.getComplexX()>0 && postmp.getComplexX()<=100){
+    pos->setComplexX(postmp.getComplexX());
+    vit->setComplexX(postmp.getComplexX()+tr.getComplexX());
+    }
+    else{
+        vit->setComplexX(pos->getComplexX()+tr.getComplexX());
+    }
+    if(postmp.getComplexY()>=0 && postmp.getComplexY()<=100){
+    pos->setComplexY(postmp.getComplexY());
+    vit->setComplexY(postmp.getComplexY()+tr.getComplexY());
+    }
+    else{
+        vit->setComplexY(pos->getComplexY()+tr.getComplexY());
+    }
+    printf("Position: ");
+    printf("%f \n", pos->getComplexX());
+    printf("%f \n", pos->getComplexY());
+    printf("Vitesse: ");
+    printf("%f \n", vit->getComplexX());
+    printf("%f \n", vit->getComplexY());
 }
 
 void personnage::update(){
-    destRect.x=pos->getComplexX();
-    destRect.y=pos->getComplexY();
+
 }
