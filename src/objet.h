d23593d5f4a71e5e0a4d546a14f04733da9204ad
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-04-07 11:23:37 +0200

    ajout classe objet

---
+++ src/objet.h
@@ -0,0 +1,37 @@
+#ifndef OBJET_H
+#define OBJET_H
+
+#include "def.h"
+#include "texture.h"
+
+class objet
+{
+protected:
+    bool pop;
+    texture *s = new texture();
+    float posX;
+    float posY;
+    const char* name;
+
+public:
+
+    objet();
+
+    bool getPop()const;
+    void setPop(bool b);
+
+    float getPosX();
+    float getPosY();
+    void setPos(float x, float y);
+
+    // SDL_Texture *getTexture();
+    void setTexture(SDL_Renderer *ren, const char* file, float x, float y);
+
+    SDL_Rect* getRect() ;
+
+    void setFile(const char* file);
+    const char* getFileName();
+
+    ~objet();
+};
+#endif 
