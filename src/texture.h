9332b64373d4210124abc35f5d4b12790ff535dd
William <william.escriva@etu.univ-lyon1.fr>
2020-04-08 23:03:30 +0200

    fix dest

--- src/texture.h
+++ src/texture.h
@@ -16,7 +16,7 @@ public:
     SDL_Texture* loadTexture(const char *file, SDL_Renderer *ren);
     SDL_Texture* loadTextureText(const char * texte, TTF_Font *font, SDL_Color color, SDL_Renderer *ren);
     void renderTexture(SDL_Texture*tex, SDL_Renderer *ren,SDL_Rect src, SDL_Rect dst);
-    void renderTexture(SDL_Texture*tex, SDL_Renderer *ren, SDL_Rect dst, float x, float y);
+    void renderTexture(SDL_Texture*tex, SDL_Renderer *ren, SDL_Rect dst);
     
     const char *getFileName();
     void setFileName(char * file);
