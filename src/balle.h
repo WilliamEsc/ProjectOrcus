d430684ba16e0dd5ac8e4bf6d64b05c1bb3430ba
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-04-15 15:00:35 +0200

    Mouvement balle

--- src/balle.h
+++ src/balle.h
@@ -2,18 +2,45 @@
 #define BALLE_H
 
 #include "objet.h"
+#include "complex.h"
+#include "personnage.h"
 
 class balle : public objet
 {
-    private: 
+private:
+    Complex* pos;
+    Complex* vit;
+    Complex* tr ;
+    double angle;
 
-    public: 
+    personnage j ;
+    float f ;
 
+    bool b1;
+    bool b2; 
+    float distanceX ;
+    float distanceY ;
+
+public:
     balle();
-    
+
     ~balle();
 
-    void tireBalle(int x, int y, SDL_Renderer *ren);
+    Complex* getPos() const;
+    Complex* getVit() const;
+    double getAngle()const;
+    float getDistanceX();
+    float getDistanceY();
+    bool getB1();
+    bool getB2();
+    float getPosX()const;
+    float getPosY()const;
+
+    void ajoutAngle(double deg);
+
+    void updateBalle();
+
+    void tireBalle();
 };
 
 #endif
