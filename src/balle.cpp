da2f964f68dd26c0a338f55ba96146d92829c846
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-04-15 15:00:30 +0200

    Mouvement balle

--- src/balle.cpp
+++ src/balle.cpp
@@ -1,13 +1,94 @@
 #include "balle.h"
 
-balle :: balle() 
+balle ::balle()
 {
-    setPos(400,300);
+    // setPos(400, 300);
+    pos = j.getPos();
+    vit = j.getVit();
+    o->setDest(j.getRect());
+    angle = 0;
+    tr = new Complex(0, 0);
+    f = 1;
+    b1 = false;
+    b2 = false;
 }
 
-balle :: ~balle() {}
+balle ::~balle() {}
 
-void balle :: tireBalle(int x,int y,SDL_Renderer *ren)
+Complex *balle::getPos() const
 {
-    o->renderTexture(o->getTexture(),ren,o->getRect2(),x,y);
+    return pos;
+}
+
+// float balle :: getPosX() const
+// {
+//     return pos.getComplexX();
+// }
+
+// float balle :: getPosY() const
+// {
+//     return pos.getComplexY();
+// }
+
+Complex *balle::getVit() const
+{
+    return vit;
+}
+
+double balle::getAngle() const
+{
+    return angle;
+}
+
+float balle ::getDistanceX()
+{
+    return distanceX;
+}
+
+float balle ::getDistanceY()
+{
+    return distanceY;
+}
+
+bool balle ::getB1()
+{
+    return b1;
+}
+
+bool balle ::getB2()
+{
+    return b2;
+}
+
+void balle::ajoutAngle(double deg)
+{
+    Complex e(cos(-deg * 3.14 / 180), sin(-deg * 3.14 / 180));
+    *vit = ((*vit - *pos) * e) + *pos;
+    angle += deg;
+}
+
+void balle ::tireBalle()
+{
+    float x;
+    float y;
+
+    //déplacement par rapport position écran
+
+    *tr = (*vit - *pos);
+
+    *pos = *pos + *tr * f;
+    *vit = *vit + *tr * f;
+
+    //déplacement par rapport position carte
+
+    x = o->getRectX() + tr->getComplexX() * f;
+    y = o->getRectY() + tr->getComplexY() * f;
+
+    o->setDest(x, y);
+}
+
+// o->renderTexture(o->getTexture(),ren,o->getRect2());
+
+void balle::updateBalle()
+{
 }
