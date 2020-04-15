70da43eaf6c82de9f0cbeda2a3c3a4bd29ee7ae8
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-04-15 15:01:19 +0200

    maj

--- src/objet.h
+++ src/objet.h
@@ -7,11 +7,14 @@
 class objet
 {
 protected:
-    bool pop;
+    
     texture *o = new texture();
+    SDL_Rect src ;
+
     float posX;
     float posY;
     const char* name;
+    bool pop;
 
 public:
 
@@ -25,7 +28,8 @@ public:
     void setPos(float x, float y);
 
     // SDL_Texture *getTexture();
-    void setTexture(SDL_Renderer *ren, const char* file, float x, float y);
+    void setTexture(SDL_Renderer *ren, const char* file);
+    void renderTexture(SDL_Renderer *ren);
 
     SDL_Rect* getRect() ;
 
