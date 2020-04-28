27aaa94125c71ae160cb6278dabe3ca9f63d1448
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-04-28 16:04:11 +0200

    maj

--- src/texture.h
+++ src/texture.h
@@ -1,43 +1,37 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
-#include "def.h" 
+#include "def.h"
 
 class texture
 {
 private:
-    
-   const char *fileName ;
-   SDL_Texture * tex;
-   SDL_Rect dest; 
+    const char *fileName;
+    SDL_Texture *tex;
+    SDL_Rect dest;
 
 public:
-    
     texture();
-    SDL_Texture* loadTexture(const char *file, SDL_Renderer *ren);
-    SDL_Texture* loadTextureText(const char * texte, TTF_Font *font, SDL_Color color, SDL_Renderer *ren);
-    void renderTexture(SDL_Texture*tex, SDL_Renderer *ren,SDL_Rect src, SDL_Rect dst);
-    void renderTexture(SDL_Texture*tex, SDL_Renderer *ren, SDL_Rect dst);
-        void renderTextureSrc(SDL_Texture*tex, SDL_Renderer *ren, SDL_Rect src);
-    void renderTextureCplx(SDL_Texture*tex, SDL_Renderer *ren, float posx,float posy);
-    
+    SDL_Texture *loadTexture(const char *file, SDL_Renderer *ren);
+    SDL_Texture *loadTextureText(const char *texte, TTF_Font *font, SDL_Color color, SDL_Renderer *ren);
+    void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect src, SDL_Rect dst);
+    void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst);
+    void renderTextureSrc(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect src);
+    void renderTextureCplx(SDL_Texture *tex, SDL_Renderer *ren, float posx, float posy);
+
     const char *getFileName();
-    void setFileName(char * file);
+    void setFileName(char *file);
 
     SDL_Texture *getTexture();
     void setTexture(SDL_Texture *t);
 
-    SDL_Rect* getRect();
+    SDL_Rect *getRect();
     SDL_Rect getRect2();
     int getRectX();
     int getRectY();
 
-    void setDest(float x,float y, float w, float h);
+    void setDest(float x, float y, float w, float h);
     void setDest(float x, float y);
     void setDest(const SDL_Rect rect);
-
 };
-
-
-
 
 #endif
