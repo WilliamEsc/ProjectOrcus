d610333502ccc739a79935284f66990c7a6811fd
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-04-28 16:03:45 +0200

    maj

--- src/game.cpp
+++ src/game.cpp
@@ -2,66 +2,67 @@
 
 game::game(){};
 game::~game(){};
- 
-void game::init(const char* title, int posX, int posY, int width, int height, bool fullscreen)
+
+void game::init(const char *title, int posX, int posY, int width, int height, bool fullscreen)
 {
     int flags = 0;
-    if(fullscreen)
+    if (fullscreen)
     {
         flags = SDL_WINDOW_FULLSCREEN;
     }
-   
-    if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
     {
         std::cout << "Subsystems Initialised! ..." << std::endl;
 
         window = SDL_CreateWindow(title, posX, posY, width, height, flags);
-        if(window)
+        if (window)
         {
             std::cout << " window created! " << std::endl;
         }
         renderer = SDL_CreateRenderer(window, -1, 0);
-        if(renderer)
+        if (renderer)
         {
             SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
             std::cout << " renderer created! " << std::endl;
         }
 
-        isRunning = true ;
-    } else {
-        isRunning = false ;
+        isRunning = true;
     }
-    
-    if(TTF_Init() < 0)
+    else
+    {
+        isRunning = false;
+    }
+
+    if (TTF_Init() < 0)
     {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", TTF_GetError());
     }
-    
-    // Initialisation joueur 
-    SDL_Surface* tmpsurface=IMG_Load("Data/persos1.png");
-    joueur.setTexture(renderer,tmpsurface);
+
+    // Initialisation joueur
+    SDL_Surface *tmpsurface = IMG_Load("Data/persos1.png");
+    joueur.setTexture(renderer, tmpsurface);
     SDL_FreeSurface(tmpsurface);
-    
-    //initialisation de la map 
+
+    //initialisation de la map
     map m2(renderer);
-    m=m2;
+    m = m2;
 
     //Initialisation du kit de soin
-    s.setPos(25,25);
+    s.setPos(25, 25);
     s.setFile("Data/kitDeSoin.png");
-    s.setTexture(renderer,"Data/kitDeSoin.png");
+    s.setTexture(renderer, "Data/kitDeSoin.png");
     // t = new text() ;
 
     //Initialisation de la balle
-    //b.setFile("Data/balle.png");
-    //b.setTexture(renderer,"Data/balle.png");
+    b.LoadBalle(renderer);
 }
 
 // void game::handleEvents()
 // {
 //     SDL_Event events;
 //     float f=0;
-//     double ang=0; 
+//     double ang=0;
 //     while(SDL_PollEvent(&events))
 //     {
 //         if (events.type == SDL_QUIT) isRunning=false;// Si l'utilisateur a clique sur la croix de fermeture
@@ -104,44 +105,55 @@ void game::handleEvents()
 {
     SDL_Event events;
     SDL_PollEvent(&events);
-    float f=0;
-    double ang=0;
-    const Uint8* keystates;
+    float f = 0;
+    double ang = 0;
+    const Uint8 *keystates;
     keystates = SDL_GetKeyboardState(NULL);
-    
-    if (keystates[SDL_SCANCODE_W]) {
-        f=-0.01;
-    }
-    if (keystates[SDL_SCANCODE_S]) {
-        f=0.01;
-    }
-    if (keystates[SDL_SCANCODE_A]) {
-        ang=0.6;
-    }
-    if (keystates[SDL_SCANCODE_D]) {
-       ang=-0.6;
-    }
-    if (keystates[SDL_SCANCODE_ESCAPE]){
-        isRunning=false;
-    }
-    joueur.deplace(f,ang,m.collision,renderer);
 
-    if (events.type == SDL_QUIT) isRunning=false;// Si l'utilisateur a clique sur la croix de fermeture
+    if (keystates[SDL_SCANCODE_W])
+    {
+        f = -0.01;
+    }
+    if (keystates[SDL_SCANCODE_S])
+    {
+        f = 0.01;
+    }
+    if (keystates[SDL_SCANCODE_A])
+    {
+        ang = 0.6;
+    }
+    if (keystates[SDL_SCANCODE_D])
+    {
+        ang = -0.6;
+    }
+    if (keystates[SDL_SCANCODE_ESCAPE])
+    {
+        isRunning = false;
+    }
+    joueur.deplace(f, ang, m.collision, renderer);
+
+    if (events.type == SDL_QUIT)
+        isRunning = false; // Si l'utilisateur a clique sur la croix de fermeture
     if (keystates[SDL_SCANCODE_X])
     {
-         //s.setPointDeVie(s.getPointDeVie()-10);
+        //s.setPointDeVie(s.getPointDeVie()-10);
         //std::cout << "Point de vie : " << s.getPointDeVie() << std::endl ;
-        std::cout << "pdv : " << joueur.getPdv() << std::endl ;
+        std::cout << "pdv : " << joueur.getPdv() << std::endl;
 
-        std::cout << "position perso : " << joueur.getPos()->getComplexX() <<" "<< joueur.getPos()->getComplexY() << std::endl ;
-        std::cout << "position kit x: " << s.getPosX() << "y :"<< s.getPosY() << std::endl ;
+        std::cout << "position perso : " << joueur.getPos()->getComplexX() << " " << joueur.getPos()->getComplexY() << std::endl;
+        std::cout << "position kit x: " << s.getPosX() << "y :" << s.getPosY() << std::endl;
     }
-    if(events.type == SDL_MOUSEBUTTONDOWN)
+    if (events.type == SDL_MOUSEBUTTONDOWN)
     {
         int x, y;
         SDL_GetMouseState(&x, &y);
         // std::cout << "position souris : " << x << ", "<< y <<std::endl ;
         // b.setTexture(renderer,"Data/balle.png");
+    }
+    if (keystates[SDL_SCANCODE_SPACE])
+    {
+        b.setFire(true);
+        b.setAngle(joueur.getAngle());
     }
 }
 
@@ -152,34 +164,36 @@ void game::handleEvents()
 
 void game::update()
 {
-    Complex pos= *joueur.getPos();
-    Complex posS(s.getPosX(),s.getPosY());
+    Complex pos = *joueur.getPos();
+    Complex posS(s.getPosX(), s.getPosY());
     pos.Soustrait(&posS);
-    if(pos.norme()<1 && s.getPop())
+    if (pos.norme() < 1 && s.getPop())
     {
-        joueur.setPdv(joueur.getPdv()+10);
+        joueur.setPdv(joueur.getPdv() + 10);
         s.setPop(false);
     }
     //b.tireBalle();
-    
 }
 
 void game::render()
 {
     SDL_RenderClear(renderer);
     //this is where we would add stuff to render
-    m.drawMap(renderer,*joueur.getPos());
+    m.drawMap(renderer, *joueur.getPos());
 
-    if(s.getPop()){
+    if (s.getPop())
+    {
         SDL_Rect testRct;
-        testRct.x=(s.getPosX()-joueur.getPos()->getComplexX()+6)*64;
-        testRct.y=(s.getPosY()-joueur.getPos()->getComplexY()+5)*64;
-        testRct.h=testRct.w=64;
-        float x=(s.getPosX()-joueur.getPos()->getComplexX()+6)*64;
-        float y=(s.getPosY()-joueur.getPos()->getComplexY()+5)*64;
-        s.drawObjet(renderer,x,y);
-        SDL_RenderDrawRect(renderer,&testRct);
+        testRct.x = (s.getPosX() - joueur.getPos()->getComplexX() + 6) * 64;
+        testRct.y = (s.getPosY() - joueur.getPos()->getComplexY() + 5) * 64;
+        testRct.h = testRct.w = 64;
+        float x = (s.getPosX() - joueur.getPos()->getComplexX() + 6) * 64;
+        float y = (s.getPosY() - joueur.getPos()->getComplexY() + 5) * 64;
+        s.drawObjet(renderer, x, y);
+        SDL_RenderDrawRect(renderer, &testRct);
     }
+
+    b.updateBalle(renderer,b.getFire());
     //b.renderTexture(renderer);
 
     // t.setTexte("Point de vie",renderer,0,0);
@@ -187,7 +201,7 @@ void game::render()
     // t.setTexte(txtPdv.c_str(),renderer,0,20);
     //SDL_RenderCopy(renderer,joueur.getTexture(),NULL,joueur.getRect());
 
-    SDL_RenderCopyEx(renderer,joueur.getTexture(),NULL,joueur.getRect(),-joueur.getAngle(),NULL,SDL_FLIP_NONE);
+    SDL_RenderCopyEx(renderer, joueur.getTexture(), NULL, joueur.getRect(), -joueur.getAngle(), NULL, SDL_FLIP_NONE);
 
     // TTF_CloseFont(t.getFont());
     SDL_RenderPresent(renderer);
@@ -202,11 +216,10 @@ void game::clean()
     SDL_DestroyRenderer(renderer);
     TTF_Quit();
     SDL_Quit();
-    std::cout << " Game Cleaned " << std::endl ;
+    std::cout << " Game Cleaned " << std::endl;
 }
 
 bool game::running()
 {
     return isRunning;
 }
-
