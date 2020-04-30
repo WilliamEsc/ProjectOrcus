f5afd045bcff8b4e9911667771db0c0ce88175b7
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-04-30 18:17:37 +0200

    module des objets

--- src/objet.h
+++ src/objet.h
@@ -15,21 +15,23 @@ protected:
 public:
 
     objet();
+    objet(float x,float y);
 
     bool getPop()const;
     void setPop(bool b);
 
     void setPos(float x,float y);
+    Complex getPos() const;
     float getPosX() const;
     float getPosY() const;
 
-    // SDL_Texture *getTexture();
+    texture* getTexture()const;
     void setTexture(SDL_Renderer *ren, const char* file);
-    void drawObjet(SDL_Renderer *ren, float x, float y) const;
+    void drawObjet(SDL_Renderer *ren) const;
 
     SDL_Rect* getRect();
 
-    void setFile(char* file);
+    void setFile(const char* file);
     const char* getFileName();
 
     ~objet();
