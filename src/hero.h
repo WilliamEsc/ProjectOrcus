4e4d5a9bda72f1751e701cf0fa52df0be8638411
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-04-30 18:18:13 +0200

    module du hero

--- src/hero.h
+++ src/hero.h
@@ -1,4 +1,21 @@
 #ifndef HERO_H
 #define HERO_H
 
+#include "complex.h"
+#include "personnage.h"
+
+class hero: public personnage
+{
+    private:
+
+
+    public:
+
+    hero();
+    ~hero();
+
+    void ajoutAngle(const double & deg);
+    void deplace(const float & f,const double & ang,const int* collision);
+};
+
 #endif 
