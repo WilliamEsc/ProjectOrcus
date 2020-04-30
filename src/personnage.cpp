d8e8a48ddb1f9c4600f318f3a53e5961e40d7fae
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-04-30 18:19:12 +0200

    modif pour heritage

--- src/personnage.cpp
+++ src/personnage.cpp
@@ -5,36 +5,42 @@ personnage::personnage(){
     vit=new Complex(20,19);
     angle=0;
     pdv=100;
-    destRect.h=64;
-    destRect.w=64;
-    destRect.x=(SCREEN_WIDTH-64)/2;
-    destRect.y=(SCREEN_HEIGHT-64)/2;
+    persos->setDest(0,0,64,64);
 }
 
-// personnage::personnage(float x,float y){
-//     pos=new Complex(x,y);
-//     vit=new Complex(x,y-1);
-//     angle=0;
-//     pdv=100;
-//     destRect.h=64;
-//     destRect.w=64;
-//     destRect.x=(SCREEN_WIDTH-64)/2;
-//     destRect.y=(SCREEN_HEIGHT-64)/2;
-// }
+personnage::personnage(const float & x,const float & y){
+    pos=new Complex(x,y);
+    vit=new Complex(x,y-1);
+    angle=0;
+    pdv=100;
+    persos->setDest(0,0,64,64);
+}
 
 personnage::~personnage(){
     delete pos;
     delete vit;
+    delete persos;
+    persos=NULL;
     pos=NULL;
     vit=NULL;
 }
 
-SDL_Texture* personnage::getTexture()const {
-    return Pion;
+void personnage::setTexture(SDL_Renderer* renderer,const char* file){
+    persos->setFileName(file);
+    persos->setTexture(persos->loadTexture(renderer));
 }
 
-SDL_Rect* personnage::getRect(){
-    return &destRect;
+texture* personnage::getTexture()const {
+    return persos;
+}
+
+void personnage::setDest(const float & x,const float & y)
+{
+    persos->setDest(x,y);
+}
+
+SDL_Rect* personnage::getRect() const{
+    return persos->getDest();
 }
 
 Complex* personnage::getPos()const {
@@ -53,32 +59,17 @@ int personnage::getPdv()const{
     return pdv;
 }
 
-void personnage::setPdv(int n){
+void personnage::setPdv(const int & n){
     pdv=n;
 }
 
-void personnage::setTexture(SDL_Renderer* renderer,SDL_Surface* surface){
-    Pion=SDL_CreateTextureFromSurface(renderer,surface);
-    if(Pion==NULL)
-    {
-        printf("erreur Pion");
-        exit(EXIT_FAILURE);
-    }
+void personnage::drawPersonnage(SDL_Renderer* renderer)
+{
+    persos->renderTextureEx(renderer,-angle);
 }
 
-void personnage::ajoutAngle(double deg){
-    Complex e(cos(-deg*3.14/180),sin(-deg*3.14/180));
-    *vit=((*vit-*pos)*e)+*pos;
-    angle+=deg;
-}
-
-void personnage::deplace(float f,double ang,int collision[124][124],SDL_Renderer* renderer){
-    ajoutAngle(ang);
-    Complex postmp=*pos;
-    Complex tr=*vit-postmp;
-    postmp=postmp-tr*f;
-    bool collide=false;
-
+bool personnage::Collision(const Complex & postmp,const int collision[][124]) const
+{
     for(int i=postmp.getComplexY()-1;i<postmp.getComplexY()+1;i++){
         for(int j=postmp.getComplexX()-1;j<postmp.getComplexX()+1;j++){
             if(collision[i][j]!=-1){
@@ -99,26 +90,40 @@ void personnage::deplace(float f,double ang,int collision[124][124],SDL_Renderer
                       (i<postmp.getComplexY()+0.75 && i+1>postmp.getComplexY()+0.75)
                     )
                 ){
-                    collide=true;
+                    return true;
                 }
             }
         }
     }
-    
-    // printf("tmp x %f \n", postmp.getComplexX());
-    // printf("tmp y %f \n", postmp.getComplexY());
-    // printf("tr x %f \n",tr.getComplexX());
-    // printf("tr y %f \n",tr.getComplexY());
-    if(!collide){
-        pos->setComplexX(postmp.getComplexX());
-        pos->setComplexY(postmp.getComplexY());
-        vit->setComplexX(postmp.getComplexX()+tr.getComplexX());
-        vit->setComplexY(postmp.getComplexY()+tr.getComplexY());
+    return false;
+}
+
+bool personnage::Collision(const Complex & postmp,const int* collision) const
+{
+    for(int i=postmp.getComplexY()-1;i<postmp.getComplexY()+1;i++){
+        for(int j=postmp.getComplexX()-1;j<postmp.getComplexX()+1;j++){
+            if(collision[(i*124)+j]!=-1){
+                if( (///en haut a gauche
+                      (j<postmp.getComplexX()+0.25 && j+1>postmp.getComplexX()+0.25) &&
+                      (i<postmp.getComplexY()+0.25 && i+1>postmp.getComplexY()+0.25)
+                    ) ||
+                    (///en bas a droite
+                      (j<postmp.getComplexX()+0.75 && j+1>postmp.getComplexX()+0.75) &&
+                      (i<postmp.getComplexY()+0.75 && i+1>postmp.getComplexY()+0.75)
+                    ) ||
+                    (///en haut a droite
+                      (j<postmp.getComplexX()+0.75 && j+1>postmp.getComplexX()+0.75) &&
+                      (i<postmp.getComplexY()+0.25 && i+1>postmp.getComplexY()+0.25)
+                    ) ||
+                    (
+                      (j<postmp.getComplexX()+0.25 && j+1>postmp.getComplexX()+0.25) &&
+                      (i<postmp.getComplexY()+0.75 && i+1>postmp.getComplexY()+0.75)
+                    )
+                ){
+                    return true;
+                }
+            }
+        }
     }
-    // printf("Position: ");
-    // printf("pos x %f \n", pos->getComplexX());
-    // printf("pos y %f \n", pos->getComplexY());
-    // printf("Vitesse: ");
-    // printf("%f \n", vit->getComplexX());
-    // printf("%f \n", vit->getComplexY());
+    return false;
 }
