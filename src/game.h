5591cb2ca14b03920f0a95b9a5f45d973e411055
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-04-30 18:20:10 +0200

    Ajout de chaque module

--- src/game.h
+++ src/game.h
@@ -1,10 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "personnage.h"
+#include "hero.h"
+#include "ennemie.h"
 #include "def.h"
 #include "map.h"
 #include "soin.h"
+#include "cles.h"
 #include "balle.h"
 #include "text.h"
 #include <iostream>
@@ -14,13 +16,15 @@ class game
 private:
     /* data */
     bool isRunning;
-    personnage joueur;
+    hero joueur;
+    std::vector<ennemie*> target;
     SDL_Window * window;
     SDL_Renderer *renderer;
-    soin s;
+    std::vector<soin*> heal;
+    cles* key;
     balle b;
     text t;
-    map m;
+    map* m;
 
  
     
