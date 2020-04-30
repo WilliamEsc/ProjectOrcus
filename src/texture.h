f0ba85c1064ad6796f5d6e5833c5966ade416f01
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-04-30 18:18:51 +0200

    fix des fonctions render

--- src/texture.h
+++ src/texture.h
@@ -1,37 +1,48 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
-#include "def.h"
+#include "def.h" 
 
 class texture
 {
 private:
-    const char *fileName;
-    SDL_Texture *tex;
-    SDL_Rect dest;
+    
+   const char *fileName ;
+   SDL_Texture * tex;
+   SDL_Rect dest;
+   SDL_Rect src;
 
 public:
+    
     texture();
-    SDL_Texture *loadTexture(const char *file, SDL_Renderer *ren);
-    SDL_Texture *loadTextureText(const char *texte, TTF_Font *font, SDL_Color color, SDL_Renderer *ren);
-    void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect src, SDL_Rect dst);
-    void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst);
-    void renderTextureSrc(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect src);
-    void renderTextureCplx(SDL_Texture *tex, SDL_Renderer *ren, float posx, float posy);
-
+    SDL_Texture* loadTexture(SDL_Renderer *ren);
+    SDL_Texture* loadTextureText(const char * texte, TTF_Font *font, SDL_Color color, SDL_Renderer *ren);
+    void renderTextureNoDest(SDL_Renderer *ren);
+    void renderTextureNoSrc(SDL_Renderer *ren);
+    void renderTexture(SDL_Renderer *ren);
+    void renderTextureEx(SDL_Renderer *ren,double ang);
+    
     const char *getFileName();
-    void setFileName(char *file);
+    void setFileName(const char * file);
 
-    SDL_Texture *getTexture();
+    SDL_Texture *getSDLTexture();
     void setTexture(SDL_Texture *t);
 
-    SDL_Rect *getRect();
-    SDL_Rect getRect2();
-    int getRectX();
-    int getRectY();
+    SDL_Rect* getDest();
+    SDL_Rect getDest2();
+    int getDestX();
+    int getDestY();
 
-    void setDest(float x, float y, float w, float h);
+    void setDest(float x,float y, float w, float h);
     void setDest(float x, float y);
     void setDest(const SDL_Rect rect);
+
+    void setSrc(float x,float y, float w, float h);
+    void setSrc(float x, float y);
+    void setSrc(const SDL_Rect rect);
+
 };
+
+
+
 
 #endif
