6dc70ee10957f526aac579ba6e1580f236660461
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2020-05-04 01:40:51 +0200

    ajout du menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,49 @@
+#include "def.h"
+#include "texture.h"
 #include "game.h"
 
 int main(int argc, const char *argv[])
 {
 
     game g;
+    SDL_Event event;
+    bool game = true;
 
     g.init("écran", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 832, 704, false);
 
-    while (g.running()) //game is running
+    while (game) //game is running
     {
         /* handle any user input
         update all object...
         render changes to the display */
 
-        g.handleEvents();
-        g.update();
-        g.render();
+        g.renderMenu();
+
+        SDL_WaitEvent(&event);
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            game = false;
+            break;
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_ESCAPE:
+                game = false;
+                break;
+
+            case SDLK_SPACE: // On lance le jeu 
+                while (g.running())
+                {
+                    g.handleEvents();
+                    g.update();
+                    g.render();
+                }
+                g.setRunning(true);
+                break;
+            }
+            break;
+        }
     }
 
     g.clean();
