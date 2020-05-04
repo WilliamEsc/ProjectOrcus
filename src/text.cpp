88bf4667e92a92cc3db3b82002a0acc68d718921
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-05-04 13:10:38 +0200

    reintegration du texte

--- src/text.cpp
+++ src/text.cpp
@@ -1,61 +1,61 @@
-// #include "text.h"
+#include "text.h"
 
-// text::text()
-// {
-//     texte = "";
-//     color = {0, 0, 0};
-//     font = TTF_OpenFont("fonts/Life_is_goofy.ttf", 50);
-// }
+text::text()
+{
+    texte = "";
+    color = {0, 0, 0};
+    font = TTF_OpenFont("fonts/Life_is_goofy.ttf", 50);
+}
 
-// text::~text()
-// {
-//     delete t;
-// }
+text::~text()
+{
+    delete t;
+}
 
-// const char *text::getTexte()
-// {
-//     return texte;
-// }
+const char *text::getTexte()
+{
+    return texte;
+}
 
-// SDL_Color text::getColor()
-// {
-//     return color;
-// }
+SDL_Color text::getColor()
+{
+    return color;
+}
 
-// TTF_Font *text::getFont()
-// {
-//     return font;
-// }
+TTF_Font *text::getFont()
+{
+    return font;
+}
 
-// float text::getPosX()
-// {
-//     return posX;
-// }
+float text::getPosX()
+{
+    return posX;
+}
 
-// float text::getPosY()
-// {
-//     return posY;
-// }
+float text::getPosY()
+{
+    return posY;
+}
 
-// void text::setFont(TTF_Font *f)
-// {
-//     font = f;
-// }
+void text::setFont(TTF_Font *f)
+{
+    font = f;
+}
 
-// void text::setColor(SDL_Color c)
-// {
-//     color = c;
-// }
+void text::setColor(SDL_Color c)
+{
+    color = c;
+}
 
-// void text::setPosText(float x, float y)
-// {
-//     posX = x;
-//     posY = y;
-// }
+void text::setPosText(float x, float y)
+{
+    posX = x;
+    posY = y;
+}
 
-// void text::setTexte(const char *t1, SDL_Renderer *ren, float x, float y)
-// {
-//     texte = t1;
-//     t->setTexture(t->loadTextureText(texte, font, color, ren));
-//     t->renderTexture(ren);
-// }
+void text::setTexte(const char *t1, SDL_Renderer *ren, float x, float y)
+{
+    texte = t1;
+    t->setTexture(t->loadTextureText(texte, font, color, ren));
+    t->renderTexture(ren);
+}
