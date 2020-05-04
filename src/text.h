9ed8381783c78f15dacbae200492da6b79b5ade9
ESCRIVA WILLIAM p1702268 <william.escriva@etu.univ-lyon1.fr>
2020-05-04 13:10:41 +0200

    reintegration du texte

--- src/text.h
+++ src/text.h
@@ -1,34 +1,34 @@
-// #ifndef TEXT_H
-// #define TEXT_H
+#ifndef TEXT_H
+#define TEXT_H
 
-// #include "def.h"
-// #include "texture.h"
+#include "def.h"
+#include "texture.h"
 
-// class text
-// {
+class text
+{
 
-// private:
-//     const char *texte;
-//     SDL_Color color;
-//     TTF_Font *font;
-//     float posX;
-//     float posY;
-//     texture *t = new texture();
+private:
+    const char *texte;
+    SDL_Color color;
+    TTF_Font *font;
+    float posX;
+    float posY;
+    texture *t = new texture();
 
-// public:
-//     text();
-//     ~text();
+public:
+    text();
+    ~text();
 
-//     const char *getTexte();
-//     SDL_Color getColor();
-//     TTF_Font *getFont();
-//     float getPosX();
-//     float getPosY();
+    const char *getTexte();
+    SDL_Color getColor();
+    TTF_Font *getFont();
+    float getPosX();
+    float getPosY();
 
-//     void setTexte(const char *t1, SDL_Renderer *ren, float x, float y);
-//     void setColor(SDL_Color c);
-//     void setFont(TTF_Font *f);
-//     void setPosText(float x, float y);
-// };
+    void setTexte(const char *t1, SDL_Renderer *ren, float x, float y);
+    void setColor(SDL_Color c);
+    void setFont(TTF_Font *f);
+    void setPosText(float x, float y);
+};
 
-// #endif
+#endif
